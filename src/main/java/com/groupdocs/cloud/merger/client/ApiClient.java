4e871904ce5ba7c2fa4866dee6ad8b885f54329f
product-team <product.team@groupdocs.com>
2024-11-28 02:03:31 -0600

    Updated sources

--- src/main/java/com/groupdocs/cloud/merger/client/ApiClient.java
+++ src/main/java/com/groupdocs/cloud/merger/client/ApiClient.java
@@ -97,7 +97,7 @@ public class ApiClient {
         this.json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("java-sdk/24.7");
+        setUserAgent("java-sdk/24.11");
 
         // Set connection timeout
         setConnectTimeout(configuration.getTimeout());
